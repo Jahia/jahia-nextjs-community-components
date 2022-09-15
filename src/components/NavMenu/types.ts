f91ff56a35a9fcba1af41582c2a634603d63a2f4
Hervé Duchesne <hduchesne@jahia.com>
2022-09-15 12:03:08 +0200

    clean types

--- src/components/NavMenu/types.ts
+++ src/components/NavMenu/types.ts
@@ -1,9 +1,5 @@
-import {ComponentPropsType, GqlNode, Node} from '@jahia/nextjs-sdk';
+import {GqlNode, Node} from '@jahia/nextjs-sdk';
 
-// Export enum MenuTypeEnum {
-//     base = 'base',
-//     select = 'select'
-// }
 export type NavMenuPropsType = {
     'jcr:title'?:string
     'j:menuType':string
@@ -13,19 +9,8 @@ export type NavMenuPropsType = {
     'j:menuNodes'?:Node[]
 }
 
-// Export type NavMenuTreePropsType = {
-//     'j:baseNode': Node,
-//     'j:includeBaseNode': string,
-//     'j:maxDepth'?: string,
-//     'j:excludePages'?:Node[]
-// }
-
 export type NavMenuTreePropsType = {
     'baseTreeNode': GqlNode,
     'excludePages':string[]
 }
-
-// Export type NavMenuSetPropsType = {
-//     'menuNodes':Node[]
-// }
 
